1c3a09149fb6281bf005d223508ad112d449e0de
Ha1dex <haidexCSGO@gmail.com>
2023-02-17 23:22:36 +0300

    initial commit

--- app/src/main/java/com/example/skyeng/MainActivity.java
+++ app/src/main/java/com/example/skyeng/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.skyeng;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,4 +28,22 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Detail");
 
     }
+    @Override
+    protected void onStart()
+    {
+        super .onStart();
+        setContentView(R.layout.activity_main);
+        Context info = getApplicationContext();
+        CharSequence message = "Start activity";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(info, message, duration);
+        Toast toast = Toast.makeText(info, message, duration);
+        toast.show();
+        Log.e(TAG,"Fail");
+        Log.w(TAG, "Warning");
+        Log.i(TAG, "Information");
+        Log.d(TAG, "Debug");
+        Log.v(TAG, "Detail");
+    }
 }
+
